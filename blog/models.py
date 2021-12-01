9de96b7d8ed8137222beaa9121b30c41ef280c29
rhtm123 <maurya.iitk@gmail.com>
2021-12-01 11:01:44 +0530

    media files

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,9 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     detail = models.TextField()
     pub_date = models.DateField()
+    thumbnail = models.ImageField(upload_to="blog", null=True, blank=True)
+    likes = models.PositiveIntegerField(default=0)
+    dislikes = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
--- demoproject/urls.py
+++ demoproject/urls.py
@@ -19,6 +19,9 @@ from django.urls import path
 from django.shortcuts import HttpResponse
 from django.urls.conf import include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from blog import views
 
 urlpatterns = [
@@ -28,4 +31,4 @@ urlpatterns = [
     path("about/", views.about_view),
     path("demo-html-page/", views.demo_html),
     path("blog/", include("blog.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
